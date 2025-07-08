7f1375fc0590bdcd539135bd38cf7ff9c07f4d83
Kassa_G_Weldemariam <kassagebrekidan550@gmail.com>
2025-07-08 16:18:08 +0300

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/header/Header'
-
+import CarouselEffect from './components/carousel/CarouselEffect'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header/>
+      <Header />
+      <CarouselEffect />
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/carousel/CarouselEffect.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Carousel } from "react-responsive-carousel";
+import {img} from './img/data.js';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import classes from './carousel.module.css';
+function CarouselEffect() {
+  return (
+    <div className={classes.carousel_container}>
+      <Carousel
+        autoplay={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showIndicators={false}
+        showStatus={false}
+      >
+        {img.map((imageItemLink, index) => {
+          return <img key={index} src={imageItemLink} alt={`image-${index}`} />;
+        })}
+      </Carousel>
+      <div className={classes.hero_img}></div>
+    </div>
+  );
+}
+
+export default CarouselEffect;
