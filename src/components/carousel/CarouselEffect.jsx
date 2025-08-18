864acc97e6813b9fe0e7b231ec7dd43734576af6
Kassa_G_Weldemariam <kassagebrekidan550@gmail.com>
2025-08-18 11:29:07 +0300

    minor fixations with carousel

--- src/components/carousel/CarouselEffect.jsx
+++ src/components/carousel/CarouselEffect.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
 import { Carousel } from "react-responsive-carousel";
-import {img} from './img/data.js';
+import { img } from "./img/data.js";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
-import classes from './carousel.module.css';
+import classes from "./carousel.module.css";
 function CarouselEffect() {
   return (
     <div className={classes.carousel_container}>
       <Carousel
-        autoplay={true}
+        autoPlay={true}
         interval={3000}
         infiniteLoop={true}
         showThumbs={false}
@@ -15,7 +15,7 @@ function CarouselEffect() {
         showStatus={false}
       >
         {img.map((imageItemLink, index) => {
-          return <img key={index} src={imageItemLink} alt={`image-${index}`} />;
+          return <img key={index} src={imageItemLink} alt={`slide-${index}`} />;
         })}
       </Carousel>
       <div className={classes.hero_img}></div>
